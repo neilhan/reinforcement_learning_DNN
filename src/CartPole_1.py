a13e2abd9bb6f1e89853ddd296ae154ea6d832a0
Neil Han <neil_han@intuit.com>
2018-06-10 23:20:56 -0700

    first commit

---
+++ src/CartPole_1.py
@@ -0,0 +1,17 @@
+from __future__ import print_function, nested_scopes, generators, division
+import gym
+
+env = gym.make('CartPole-v0')
+state = env.reset()
+
+print('state:', state)
+
+done = False
+
+i = 0
+while not done:
+    env.render()
+    print('act:', i)
+    observation, reward, done, info = env.step(env.action_space.sample())  # random action
+    i += 1
+print('random done')
