868d7363b5600579e2df6217754631562e0da43f
Neil Han <neil.han@gmail.com>
2021-01-14 20:11:37 -0800

    add jupyterlab

---
+++ src/reversi/players/RandomPlayer.py
@@ -0,0 +1,13 @@
+import random
+
+from .. import GameBoard
+
+
+class RandomPlayer:
+
+    def __init__(self, player_id):
+        self.player_id = player_id
+
+    def pick_next_move(self, board: GameBoard.GameBoard) -> GameBoard.Spot:
+        choices = board.get_valid_spots(self.player_id)
+        return random.choice(choices)
